0c006cc45579ddf0f707b39d7954f78066b5f675
Lâm Minh Thành <lmt151099@gmail.com>
2019-06-04 17:49:04 +0700

    render signup form, signin form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+router.get('/signin', (req, res) => {
+  res.render('signin')
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router;
