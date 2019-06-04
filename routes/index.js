0fc7bd771cf732ed0e5207e4773dbdefc69cad7f
Lâm Minh Thành <lmt151099@gmail.com>
2019-06-04 18:12:44 +0700

    Fix render signin, signup form

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 router.get('/signin', (req, res) => {
-  res.render('signin')
-})
-router.get('/register', (req, res) => {
-  res.render('register')
-})
+  res.render('signin', { title: 'Signin' });
+});
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Signup' });
+});
 
 module.exports = router;
