186012ef1e9fe2562c73d7252bd08402cfc3bbd9
Lâm Minh Thành <lmt151099@gmail.com>
2019-06-04 21:45:07 +0700

    user register

--- app.js
+++ app.js
@@ -2,39 +2,48 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 dotenv.config();
 const app = express();
+
+//db
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+.then(() => console.log('DB connected'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
+// use routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { getSignin, getSignup, signin, signup } = require('../controllers/auth');
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Home' });
+router.get('/', (req, res) => {
+  res.send('hello')
 });
-router.get('/signin', (req, res) => {
-  res.render('signin', { title: 'Signin' });
-});
-router.get('/signup', (req, res) => {
-  res.render('signup', { title: 'Signup' });
-});
+router.get('/signin', getSignin);
+router.get('/signup', getSignup);
+router.post('/signin', signin);
+router.post('/signup', signup);
 
 module.exports = router;
